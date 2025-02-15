8f444026b748071e356aca7c86f53ed8f19bdc36
alxrgdriguez <sralexred@gmail.com>
2025-02-15 21:54:45 +0100

    Funcionado_FaltaUbicaciones

--- Library/resources/views/components/layouts/layout-index.blade.php
+++ Library/resources/views/components/layouts/layout-index.blade.php
@@ -23,7 +23,7 @@
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 mr-3" viewBox="0 0 20 20" fill="currentColor">
                     <path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z" />
                 </svg>
-                Laravel Library
+                La Biblioteca de Alejandro
             </a>
 
             <nav>
@@ -48,7 +48,7 @@
 <!-- Pie de página -->
 <footer class="bg-gray-800 text-white py-8 mt-8">
     <div class="border-gray-700 mt-2 pt-2 text-center font-bold">
-        <p>&copy; 2025 Laravel Library. Todos los derechos reservados.</p>
+        <p>&copy; 2025 La Biblioteca de Alejandro. Todos los derechos reservados.</p>
     </div>
 </footer>
 </body>
